2b0670b7897d032098b5077b25d2d5ebefd15ffd
Billings, Blair <billings.blair@principal.com>
2014-08-21 18:26:39 -0500

    row of jobs concept

--- index.php
+++ index.php
@@ -14,17 +14,65 @@
 	</head>
 	<body>
 		<div class="container">
-			<div class="row">
+			<div class="row top-header">
 				<div class="col-md-3">
 					<img data-src="holder.js/200x200" class="img-responsive img-circle" alt="200x200" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIj48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iIzBEOEZEQiIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjEwMCIgeT0iMTAwIiBzdHlsZT0iZmlsbDojZmZmO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjEzcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+MjAweDIwMDwvdGV4dD48L3N2Zz4=">
 				</div>
 				<div class="col-md-9">
 					<h1>Blair Billings</h1>
 					<p class="text-muted small">Application Developer.<br>Production Specialis.<br>Sports Enthusiast.</p>
+					<br>
+				</div>
+			</div>	
+			<br>		
+			<div class="row row-bottom-border">
+				<div class="col-md-9">
+					<h4>The Principal Financial Group</h4>
+					<ul>
+						<li>lorem ipsum</li>
+						<li>fsds sdfasdf cvsdfasdf</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+					<small class="text-muted">(Dec. 2012 - Present)</small>
 				</div>
 			</div>
-			<div class="row"></div>
-			<div class="row"></div>
+			<div class="row row-bottom-border">
+				<div class="col-md-9">
+					<h4>USDA Center for Veterinary Biologics</h4>
+					<ul>
+						<li>lorem ipsum</li>
+						<li>fsds sdfasdf cvsdfasdf</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+					<small class="text-muted">(Jan. 2011 - Dec. 2012)</small>
+				</div>
+			</div>
+			<div class="row row-bottom-border">
+				<div class="col-md-9">
+					<h4>HNI Corporation</h4>
+					<ul>
+						<li>lorem ipsum</li>
+						<li>fsds sdfasdf cvsdfasdf</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+						<li>zxcvxcv  sdfasfe sdcsdc dsfaesfd</li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+					<small class="text-muted">(Jan. 2011 - Dec. 2012)</small>
+				</div>
+			</div>
 		</div>
 		<div class="footer">
 			<div class="container">
