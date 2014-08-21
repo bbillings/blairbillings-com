b9eea9a562d7e0e9acd7b76baeb8dfc18a0120a5
Billings, Blair <billings.blair@principal.com>
2014-08-21 17:32:10 -0500

    footer

--- index.php
+++ index.php
@@ -2,10 +2,28 @@
 <html lang="en">
 	<head>
 		<title>Blair Billings</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/main.css">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		Hello World!
-		checking for deploy...
-		still checking...
+		<div class="container">
+		
+		</div>
+		<div class="footer">
+			<div class="container">
+				<div class="row text-center small">
+  					<div class="col-md-4"><a href="" target="_blank">@bmbillings</a></div>
+  					<div class="col-md-4"><a href="">LinkedIn</a></div>
+  					<div class="col-md-4"><a href="mailto:bbill7@gmail.com"><span class="glyphicon glyphicon-envelope"></span>&nbsp;bbill7@gmail.com</a></div>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
