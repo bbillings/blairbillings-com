96f5baa53c9be706058dfa57573f94cfb7e43223
Billings, Blair <billings.blair@principal.com>
2014-08-21 17:39:31 -0500

    fixed links

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html lang="en">
 	<head>
 		<title>Blair Billings</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- Latest compiled and minified CSS -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">		
+		<link rel="stylesheet" href="css/main.css">
+		
+		<!-- Latest compiled and minified JavaScript -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="css/main.css">
-		<!-- Latest compiled and minified JavaScript -->
 		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
@@ -19,9 +19,9 @@
 		<div class="footer">
 			<div class="container">
 				<div class="row text-center small">
-  					<div class="col-md-4"><a href="" target="_blank">@bmbillings</a></div>
-  					<div class="col-md-4"><a href="">LinkedIn</a></div>
-  					<div class="col-md-4"><a href="mailto:bbill7@gmail.com"><span class="glyphicon glyphicon-envelope"></span>&nbsp;bbill7@gmail.com</a></div>
+  					<div class="col-md-4"><a href="http://www.twitter.com/bmbillings" target="_blank">@bmbillings</a></div>
+  					<div class="col-md-4"><a href="https://www.linkedin.com/profile/view?id=356298611" target="_blank">LinkedIn</a></div>
+  					<div class="col-md-4"><a href="mailto:bbill7@gmail.com">bbill7@gmail.com</a></div>
 				</div>
 			</div>
 		</div>
